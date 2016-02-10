c40ce4992abb487c8d2f538e091982aecae22ffa
haanj <joshua.haan@ymail.com>
2016-02-09 20:30:05 -0800

    routing completed for index

--- class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/aboutController.js
@@ -3,6 +3,7 @@
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
+    console.log('testtesttestfuckyeah');
 
   };
 
